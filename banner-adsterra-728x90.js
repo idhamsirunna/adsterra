042f2358d71e116c2850d8b837414f90648771cd
Idham Sirunna <idham.sirunna@gmail.com>
2022-12-05 07:12:21 +0800

    Create banner-adsterra-728x90.js

---
+++ banner-adsterra-728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '3f91041df6101b1b3358cdb949ed3116',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://kalganautographeater.com/3f91041df6101b1b3358cdb949ed3116/invoke.js"></scr' + 'ipt>');
