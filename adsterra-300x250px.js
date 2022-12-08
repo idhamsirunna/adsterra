3199e05e3a51c1827a5a7341bfad6e2afbc8d24c
Idham Sirunna <idham.sirunna@gmail.com>
2022-12-09 06:03:16 +0800

    Create adsterra-300x250px.js

---
+++ adsterra-300x250px.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'da50b0a6ee0023ee46671b43267c19ce',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://kalganautographeater.com/da50b0a6ee0023ee46671b43267c19ce/invoke.js"></scr' + 'ipt>');
